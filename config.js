b874a5a55d351063fbdbbae238cdfd5a3c1ab64f
GUEVE-CRASH <guevaradimanche7@gmail.com>
2025-01-11 11:40:19 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "SA1UEapL#OJbym7y_h7YbmMyeo1zmT99sxneXfCmzbT7dqYgk_5A",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Qp22bZ1/IMG-20241129-WA0428.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://img101.pixhost.to/images/496/554321573_vanzzhoat.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
-SUDO_NB: process.env.SUDO_NB || "94742398003",
+SUDO_NB: process.env.SUDO_NB || "50948065506",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
